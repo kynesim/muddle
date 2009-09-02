fd13b3153db1cb7bdba8c3ac8a9a207ed2f306ee
Tibs <tibs@tibsnjoan.co.uk>
2009-09-02 10:07:13 +0000

    Fix issue 38 by 'cd'ing into the correct directory before doing "bzr unlink". Since this means using self.checkout_name (from the superclass), change the "local" self.co_path to self.checkout_path as well. Also add a comment explaining what 'bzr unlink' does, and make the 'bzr co' be a more explicit 'bzr checkout' (which is more obvious for the uninitiated, or those of us who can type).
    git-svn-id: https://muddle.googlecode.com/svn/trunk/muddle@175 837abeb0-121e-11de-ab22-55a8a853a75f

--- muddled/vcs/bazaar.py
+++ muddled/vcs/bazaar.py
@@ -22,33 +22,36 @@ class Bazaar(VersionControlHandler):
             raise utils.Error("Cannot extract repository URL from %s, co %s"%(repo, rel))
 
         self.bzr_repo = sp[0]
-        self.co_path = self.get_checkout_path(None)
+        self.checkout_path = self.get_checkout_path(None)
 
     def path_in_checkout(self, rel):
         return conventional_repo_path(rel)
 
     def check_out(self):
         # Check out.
-        utils.ensure_dir(self.co_path)
-        os.chdir(self.co_path)
-        utils.run_cmd("bzr co %s %s %s"%(self.r_option(), self.bzr_repo, self.checkout_name))
-        os.chdir(self.co_path)
+        utils.ensure_dir(self.checkout_path)
+        os.chdir(self.checkout_path)
+        utils.run_cmd("bzr checkout %s %s %s"%(self.r_option(), self.bzr_repo, self.checkout_name))
+        # Tell bzr that when we "commit" in this checkout, we don't want
+        # bzr to automatically "push" to the remote repository (which is
+        # its default behaviour)
+        os.chdir(os.path.join(self.checkout_path,self.checkout_name))
         utils.run_cmd("bzr unbind")
 
     def pull(self):
-        os.chdir(self.co_path)
+        os.chdir(self.checkout_path)
         utils.run_cmd("bzr pull %s"%self.bzr_repo)
 
     def update(self):
-        os.chdir(self.co_path)
+        os.chdir(self.checkout_path)
         utils.run_cmd("bzr update", allowFailure = True)
 
     def commit(self):
-        os.chdir(self.co_path)
+        os.chdir(self.checkout_path)
         utils.run_cmd("bzr commit")
 
     def push(self):
-        os.chdir(self.co_path)
+        os.chdir(self.checkout_path)
         utils.run_cmd("bzr push %s"%self.bzr_repo)
 
     def must_update_to_commit(self):

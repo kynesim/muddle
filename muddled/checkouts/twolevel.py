9e519468a69b637df1ac0b5a4f59a8414e6f339d
Tibs <tibs@tibsnjoan.co.uk>
2009-10-18 16:44:26 +0000

    Fixing issue 56, make the checkouts.twolevel.twolevel remember the actual target directory whether repo_relative is specified or not.
    Also, add "relative" as the name for this function, with "twolevel"
    retained for compatibility.o
    
    Also, add a bit of documentation
    
    
    git-svn-id: https://muddle.googlecode.com/svn/trunk/muddle@196 837abeb0-121e-11de-ab22-55a8a853a75f

--- muddled/checkouts/twolevel.py
+++ muddled/checkouts/twolevel.py
@@ -10,17 +10,45 @@ import muddled.version_control as version_control
 import urlparse
 import os
 
-def twolevel(builder, co_dir, co_name, repo_relative = None, rev = None):
+def relative(builder, co_dir, co_name, repo_relative = None, rev = None):
     """
     A two-level version of checkout.simple.relative().
+
+    In the normal case, the location in the repository and in the checkout
+    is assumed the same (i.e., <co_dir>/<co_name>). So, for instance, the
+    repository might have::
+
+        <repo>/A/B
+
+    checked out into::
+
+        src/A/B
+
+    Occasionally, though, the repository is organised differently, so for
+    instance, one might want to checkout::
+
+        <repo>/B
+
+    into::
+
+        src/A/B
+
+    In this latter case, one can use the 'repo_relative' argument, to say where
+    the checkout is relative to the repository's "base". So, in the example
+    above:
+
+        * co_dir = "A"
+        * co_name = "B"
+        * repo_relative = B
     """
     repo = builder.invocation.db.repo.get()
 
     if (repo_relative is None):
         rest = os.path.join(co_dir, co_name)
-        builder.invocation.db.set_checkout_path(co_name, os.path.join(co_dir, co_name))
     else:
         rest = repo_relative
+
+    builder.invocation.db.set_checkout_path(co_name, os.path.join(co_dir, co_name))
 
     vcs_handler = version_control.vcs_dependable_for(builder,
                                                      co_name, 
@@ -30,7 +58,9 @@ def twolevel(builder, co_dir, co_name, repo_relative = None, rev = None):
     pkg.add_checkout_rules(builder.invocation.ruleset,
                            co_name, 
                            vcs_handler)
-    
+
+# For historical reasons
+twolevel = relative   
 
 def absolute(builder, co_dir, co_name, repo_url, rev = None):
     """

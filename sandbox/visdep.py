cad4f15516fc3cb9449be2e47082b6ee8a5be190
Tibs <tibs@tibsnjoan.co.uk>
2011-06-17 09:01:32 +0000

    Make sure to delete the intermediate .dot files
    git-svn-id: https://muddle.googlecode.com/svn/trunk/muddle@628 837abeb0-121e-11de-ab22-55a8a853a75f

--- sandbox/visdep.py
+++ sandbox/visdep.py
@@ -23,6 +23,10 @@ Where <switches> are:
 
   -v[erbose]        The programs being run and the names of the intermediate
                     dot files will be shown.
+
+  -k[eep]           Keep the intermediate dot file(s). If you specify this
+                    it's probably also worth using -verbose so you know what
+                    they are called.
 """
 
 from tempfile import mkstemp
@@ -31,56 +35,68 @@ import os
 import subprocess
 import sys
 
-def process(label, reduce=False, filter='dot', verbose=False):
-    fd, dotfile_path = mkstemp(suffix='.dot', prefix='visdep_', text=True)
+def process(label, reduce=False, filter='dot', keep_files=False, verbose=False):
 
     # The first program we want to run is in the sandbox with us
     thisdir = os.path.split(__file__)[0]
     visualiser = os.path.join(thisdir, 'visualise-dependencies.py')
 
+    dotfile_path1 = None
+    dotfile_path2 = None
+
     try:
-        if verbose:
-            print 'Running', visualiser, 'for', label
-            print 'Outut dot file is', dotfile_path
-        retcode = subprocess.call('%s %s'%(visualiser, label), stdout=fd, shell=True)
-        if retcode != 0:
-            print 'Error %d running %s'%(abs(retcode), visualiser)
-            return
-    except OSError as e:
-        print 'Error running %s: %s'%(visualiser, e)
-        return
-
-    os.close(fd)
-
-    # We assume that xdot.py is on our PATH
-    if reduce:
-        tred = 'tred'
-        fd2, dotfile_path2 = mkstemp(suffix='.dot', prefix='visdep_', text=True)
+        fd, dotfile_path1 = mkstemp(suffix='.dot', prefix='visdep_', text=True)
         try:
             if verbose:
-                print 'Running', tred
-                print 'Output dot file is', dotfile_path2
-            retcode = subprocess.call('%s %s'%(tred, dotfile_path), stdout=fd2, shell=True)
+                print 'Running', visualiser, 'for', label
+                print 'Outut dot file is', dotfile_path1
+            retcode = subprocess.call('%s %s'%(visualiser, label), stdout=fd, shell=True)
             if retcode != 0:
-                print 'Error %d running %s'%(abs(retcode), tred)
+                print 'Error %d running %s'%(abs(retcode), visualiser)
                 return
         except OSError as e:
-            print 'Error running %s: %s'%(tred, e)
+            print 'Error running %s: %s'%(visualiser, e)
             return
-        os.close(fd2)
-        dotfile_path = dotfile_path2
 
-    xdot = 'xdot.py'
-    try:
-        if verbose:
-            print 'Running', xdot
-        retcode = subprocess.call('%s --filter=%s %s'%(xdot, filter, dotfile_path), shell=True)
-        if retcode != 0:
-            print 'Error %d running %s'%(abs(retcode), xdot)
+        os.close(fd)
+
+        # We assume that xdot.py is on our PATH
+        if reduce:
+            tred = 'tred'
+            fd2, dotfile_path2 = mkstemp(suffix='.dot', prefix='visdep_', text=True)
+            try:
+                if verbose:
+                    print 'Running', tred
+                    print 'Output dot file is', dotfile_path2
+                retcode = subprocess.call('%s %s'%(tred, dotfile_path1), stdout=fd2, shell=True)
+                if retcode != 0:
+                    print 'Error %d running %s'%(abs(retcode), tred)
+                    return
+            except OSError as e:
+                print 'Error running %s: %s'%(tred, e)
+                return
+            os.close(fd2)
+            dotfile_path = dotfile_path2
+        else:
+            dotfile_path = dotfile_path1
+
+        xdot = 'xdot.py'
+        try:
+            if verbose:
+                print 'Running', xdot
+            retcode = subprocess.call('%s --filter=%s %s'%(xdot, filter, dotfile_path), shell=True)
+            if retcode != 0:
+                print 'Error %d running %s'%(abs(retcode), xdot)
+                return
+        except OSError as e:
+            print 'Error running %s: %s'%(xdot, e)
             return
-    except OSError as e:
-        print 'Error running %s: %s'%(xdot, e)
-        return
+    finally:
+        if not keep_files:
+            if dotfile_path1:
+                os.remove(dotfile_path1)
+            if dotfile_path2:
+                os.remove(dotfile_path2)
 
 def main(args):
 
@@ -90,6 +106,7 @@ def main(args):
 
     reduce = False
     verbose = False
+    keep_files = False
     filter = 'dot'
     label = None
 
@@ -102,6 +119,8 @@ def main(args):
             reduce = True
         elif word in ('-v', '-verbose'):
             verbose = True
+        elif word in ('-k', '-keep'):
+            keep_files = True
         elif word in ('-f', '-filter'):
             filter = args.pop(0)
         elif word[0] == '-':
@@ -117,7 +136,7 @@ def main(args):
         print __doc__
         return 1
 
-    process(label, reduce, filter, verbose)
+    process(label, reduce, filter, keep_files, verbose)
     return 0
 
 if __name__ == '__main__':

3bdc3e81638746762a4eab0840faac89591004a5
Tibs <tibs@tonyibbs.co.uk>
2011-09-21 20:15:36 +0100

    Move common test code into its own file

--- tests/test_checkouts.py
+++ tests/test_checkouts.py
@@ -23,26 +23,17 @@ import subprocess
 import sys
 import traceback
 
-this_file = os.path.abspath(__file__)
-this_dir = os.path.split(this_file)[0]
-parent_dir = os.path.split(this_dir)[0]
+from test_support import *
 
 try:
     import muddled.cmdline
-    from muddled.utils import GiveUp, normalise_dir
-    from muddled.utils import Directory, NewDirectory, TransientDirectory
 except ImportError:
     # Try one level up
-    sys.path.insert(0,parent_dir)
+    sys.path.insert(0, get_parent_file(__file__))
     import muddled.cmdline
-    from muddled.utils import GiveUp, normalise_dir
-    from muddled.utils import Directory, NewDirectory, TransientDirectory
 
-MUDDLE_BINARY = '%s muddle'%this_file
-
-# Make up for not necessarily having a PYTHONPATH that helps
-# Assume the location of muddle_patch.py relative to ourselves
-MUDDLE_PATCH_COMMAND = '%s/muddle_patch.py'%(parent_dir)
+from muddled.utils import GiveUp, normalise_dir
+from muddled.utils import Directory, NewDirectory, TransientDirectory
 
 MUDDLE_MAKEFILE = """\
 # Trivial muddle makefile
@@ -120,137 +111,6 @@ def describe_to(builder):
                                           co_dir='multilevel/inner/checkout3',
                                           co_name='alice')
 """
-
-class ShellError(GiveUp):
-    def __init__(self, cmd, retcode):
-        msg = "Shell command '%s' failed with retcode %d"%(cmd, retcode)
-        super(GiveUp, self).__init__(msg)
-        self.retcode=retcode
-
-def shell(cmd, verbose=True):
-    """Run a command in the shell
-    """
-    if verbose:
-        print '>> %s'%cmd
-    retcode = subprocess.call(cmd, shell=True)
-    if retcode:
-        raise ShellError(cmd, retcode)
-
-def get_stdout(cmd, verbose=True):
-    """Run a command in the shell, and grab its (standard) output.
-    """
-    if verbose:
-        print ">> %s"%cmd
-    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE)
-    stdoutdata, stderrdata = p.communicate()
-    retcode = p.returncode
-    if retcode:
-        raise ShellError(cmd, retcode)
-    return stdoutdata
-
-
-def muddle(args, verbose=True):
-    """Pretend to be muddle
-
-    I already know it's going to be a pain remembering that the first
-    argument is a list of words...
-    """
-    if verbose:
-        print '++ muddle %s'%(' '.join(args))
-    muddled.cmdline.cmdline(args, MUDDLE_BINARY)
-
-def git(cmd, verbose=True):
-    """Run a git command
-    """
-    shell('%s %s'%('git',cmd), verbose)
-
-def bzr(cmd, verbose=True):
-    """Run a bazaar command
-    """
-    shell('%s %s'%('bzr',cmd), verbose)
-
-def svn(cmd, verbose=True):
-    """Run a subversion command
-    """
-    shell('%s %s'%('svn',cmd), verbose)
-
-def cat(filename):
-    """Print out the contents of a file.
-    """
-    with open(filename) as fd:
-        print '++ cat %s'%filename
-        print '='*40
-        for line in fd.readlines():
-            print line.rstrip()
-        print '='*40
-
-def touch(filename, content=None, verbose=True):
-    """Create a new file, and optionally give it content.
-    """
-    if verbose:
-        print '++ touch %s'%filename
-    with open(filename, 'w') as fd:
-        if content:
-            fd.write(content)
-
-def check_files(paths, verbose=True):
-    """Given a list of paths, check they all exist.
-    """
-    if verbose:
-        print '++ Checking files exist'
-    for name in paths:
-        if os.path.exists(name):
-            if verbose:
-                print '  -- %s'%name
-        else:
-            raise GiveUp('File %s does not exist'%name)
-    if verbose:
-        print '++ All named files exist'
-
-def check_specific_files_in_this_dir(names, verbose=True):
-    """Given a list of filenames, check they are the only files
-    in the current directory
-    """
-    wanted_files = set(names)
-    actual_files = set(os.listdir('.'))
-
-    if verbose:
-        print '++ Checking only specific files exist in this directory'
-        print '++ Wanted files are: %s'%(', '.join(wanted_files))
-
-    if wanted_files != actual_files:
-        text = ''
-        missing_files = wanted_files - actual_files
-        if missing_files:
-            text += '    Missing: %s\n'%', '.join(missing_files)
-        extra_files = actual_files - wanted_files
-        if extra_files:
-            text += '    Extra: %s\n'%', '.join(extra_files)
-        raise GiveUp('Required files are not matched\n%s'%text)
-    else:
-        if verbose:
-            print '++ Only the requested files exist'
-
-def check_nosuch_files(paths, verbose=True):
-    """Given a list of paths, check they do not exist.
-    """
-    if verbose:
-        print '++ Checking files do not exist'
-    for name in paths:
-        if os.path.exists(name):
-            raise GiveUp('File %s exists'%name)
-        else:
-            if verbose:
-                print '  -- %s'%name
-    if verbose:
-        print '++ All named files do not exist'
-
-def banner(text):
-    delim = '*' * (len(text)+4)
-    print delim
-    print '* %s *'%text
-    print delim
 
 def test_svn_simple_build():
     """Bootstrap a muddle build tree.
@@ -903,6 +763,7 @@ if __name__ == '__main__':
     args = sys.argv[1:]
     if args and args[0] == 'muddle':
         # Pretend to be muddle the command line program
+        # (but note we won't catch any exceptions)
         muddle(args[1:])
     else:
         try:

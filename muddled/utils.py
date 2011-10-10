f66be2859e9aedf0e8fbfea718620b6050b0ce39
Tibs <tibs@tonyibbs.co.uk>
2011-10-10 20:13:50 +0100

    The Directory classes should set PWD
    at least, by default.

--- muddled/utils.py
+++ muddled/utils.py
@@ -1814,17 +1814,35 @@ class Directory(object):
     If 'show_popd' is true, then a message will be printed out showing the
     directory that is being returned to. An extra "wrapper" message for any
     exception being propagated out will also be shown.
+
+    If 'set_PWD' is true, then set the os.environ['PWD'] to the directory
+    that is being "cd"ed into. This emulates the behaviour of "cd" in bash.
+    Checking the value of PWD is often used to find out what directory the
+    user thinks they are in, especially in the presence of soft links in
+    directory trees.
     """
-    def __init__(self, where, stay_on_error=False, show_pushd=True, show_popd=False):
+    def __init__(self, where, stay_on_error=False, show_pushd=True,
+                 show_popd=False, set_PWD=True):
         self.start = normalise_dir(os.getcwd())
         self.where = normalise_dir(where)
         self.close_on_error = not stay_on_error
         self.show_pushd = show_pushd
         self.show_popd = show_popd
+        self.set_PWD = set_PWD
         try:
             os.chdir(self.where)
         except OSError as e:
             raise GiveUp('Cannot change to directory %s: %s\n'%(where, e))
+
+        if set_PWD:
+            if 'PWD' in os.environ:
+                self.got_old_PWD = True
+                self.old_PWD = os.environ['PWD']
+            else:
+                self.got_old_PWD = False
+                self.old_PWD = None
+            os.environ['PWD'] = self.where
+
         if show_pushd:
             print '++ pushd to %s'%self.where
 
@@ -1835,6 +1853,13 @@ class Directory(object):
 
     def close(self):
         os.chdir(self.start)
+
+        if self.set_PWD:
+            if self.got_old_PWD:
+                os.environ['PWD'] = self.old_PWD
+            else:
+                del os.environ['PWD']
+
         if self.show_popd:
             print '++ popd to  %s'%self.start
 
@@ -1876,11 +1901,17 @@ class NewDirectory(Directory):
     directory that is being returned to. An extra "wrapper" message for any
     exception being propagated out will also be shown.
 
+    If 'set_PWD' is true, then set the os.environ['PWD'] to the directory
+    that is being "cd"ed into. This emulates the behaviour of "cd" in bash.
+    Checking the value of PWD is often used to find out what directory the
+    user thinks they are in, especially in the presence of soft links in
+    directory trees.
+
     If 'show_dirops' is true, then a message will be printed out showing the
     'mkdir' command used to create the new directory.
     """
-    def __init__(self, where, stay_on_error=False,
-                 show_pushd=True, show_popd=False, show_dirops=True):
+    def __init__(self, where, stay_on_error=False, show_pushd=True,
+                 show_popd=False, set_PWD=True, show_dirops=True):
         self.show_dirops = show_dirops
         if where is None:
             where = tempfile.mkdtemp()
@@ -1895,7 +1926,8 @@ class NewDirectory(Directory):
                 # The extra spaces are to line up with 'pushd to'
                 print '++ mkdir    %s'%where
             os.makedirs(where)
-        super(NewDirectory, self).__init__(where, stay_on_error, show_pushd, show_popd)
+        super(NewDirectory, self).__init__(where, stay_on_error, show_pushd,
+                                           show_popd, set_PWD)
 
 class TransientDirectory(NewDirectory):
     """A pushd/popd directory that gets created first and deleted afterwards
@@ -1919,15 +1951,22 @@ class TransientDirectory(NewDirectory):
     directory that is being returned to. An extra "wrapper" message for any
     exception being propagated out will also be shown.
 
+    If 'set_PWD' is true, then set the os.environ['PWD'] to the directory
+    that is being "cd"ed into. This emulates the behaviour of "cd" in bash.
+    Checking the value of PWD is often used to find out what directory the
+    user thinks they are in, especially in the presence of soft links in
+    directory trees.
+
     If 'show_dirops' is true, then a message will be printed out showing the
     'mkdir' command used to create and the 'rmtree' command used to delete the
     transient directory.
     """
     def __init__(self, where=None, stay_on_error=False, keep_on_error=False,
-                 show_pushd=True, show_popd=False, show_dirops=True):
+                 show_pushd=True, show_popd=False, set_PWD=True, show_dirops=True):
         self.rmtree_on_error = not keep_on_error
         super(TransientDirectory, self).__init__(where, stay_on_error,
-                                                 show_pushd, show_popd, show_dirops)
+                                                 show_pushd, show_popd,
+                                                 set_PWD, show_dirops)
 
     def close(self, delete_tree):
         # Don't delete the tree unless asked to

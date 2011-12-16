c24e20f4d1efcd12f6f50ff44aacd97a53fd779d
Tibs <tibs@tonyibbs.co.uk>
2011-12-16 20:59:06 +0000

    This is a better way to do handlers.
    Also, conflate prefix and extension, as suffix - they're not special
    enough to separate.

--- sandbox/repository.py
+++ sandbox/repository.py
@@ -15,9 +15,9 @@ class Repository(object):
 
       >>> r = Repository('git+ssh://git@project-server/opt/kynesim/projects/042/git/',
       ...                'builds')
-      >>> r.path()
+      >>> r.repo_url
       'ssh://git@project-server/opt/kynesim/projects/042/git/builds'
-      >>> r.base_path
+      >>> r.given_path
       'git+ssh://git@project-server/opt/kynesim/projects/042/git/'
       >>> r.co_name
       'builds'
@@ -30,29 +30,41 @@ class Repository(object):
     - for instance, we have a built-in rule for google code projects:
 
       >>> g1 = Repository('git+https://code.google.com/p/grump', 'default')
-      >>> g1.path()
-      'https://code.google.com/p/grump/'
+      >>> g1.repo_url
+      'https://code.google.com/p/grump'
       >>> g2 = Repository('git+https://code.google.com/p/grump', 'wiki')
-      >>> g2.path()
-      'https://code.google.com/p/grump.wiki/'
+      >>> g2.repo_url
+      'https://code.google.com/p/grump.wiki'
+
+    which is detected automatically, and the appropriate handler used. If we
+    don't want that, we can explicitly say so:
+
+      >>> g3 = Repository('git+https://code.google.com/p/grump', 'default',
+      ...                 handler=None)
+      >>> g3.repo_url
+      'https://code.google.com/p/grump/default'
+
+    or we can ask for it explicitly by name:
+
+      >>> g4 = Repository('git+https://code.google.com/p/grump', 'default',
+      ...                 handler='code.google.com')
+      >>> g4.repo_url
+      'https://code.google.com/p/grump'
+
+    The default handler name is actually 'guess', which tries to decide by
+    looking at the repository URL - basically, if the repository starts with
+    "https://code.google.com/p/" it will use the 'code.google.com' handler,
+    and otherwise it won't.
+
+      Note: this only applies if the VCS is 'git' at the moment.
 
     Sometimes, we need some extra "path" between the repository base path and
     the checkout name. For instance:
 
       >>> r = Repository('git+ssh://git@project-server/opt/kynesim/projects/042/git/',
       ...                'busybox-1.18.5', prefix='core')
-      >>> r.path()
+      >>> r.repo_url
       'ssh://git@project-server/opt/kynesim/projects/042/git/core/busybox-1.18.5'
-
-    Git servers sometimes want us to put '.git' on the end of a checkout name.
-    This can be done as follows:
-
-        >>> r = Repository('git+git@github.com:tibs', 'withdir', extension='.git')
-        >>> r.path()
-        'git@github.com:tibs/withdir.git'
-
-    (although note that github will cope with or without the '.git' at the end).
-    Note that we had to specify the '.' in '.git', it wasn't assumed.
 
     Bazaar, in particular, sometimes wants to add trailing text to the checkout
     name, commonly to indicate a branch (bzr doesn't really support branches as
@@ -60,16 +72,28 @@ class Repository(object):
     are named). So, for instance:
 
       >>> r = Repository('bzr+ssh://bzr@project-server/opt/kynesim/projects/042/bzr/',
-      ...                'repo42', postfix='fixit_branch')
-      >>> r.path()
+      ...                'repo42', suffix='/fixit_branch')
+      >>> r.repo_url
       'ssh://bzr@project-server/opt/kynesim/projects/042/bzr/repo42/fixit_branch'
+
+    Note that we had to specify the '/' in 'suffix', it wasn't assumed.
+
+    Git servers sometimes want us to put '.git' on the end of a checkout name.
+    This can be done using the same mechanism:
+
+        >>> r = Repository('git+git@github.com:tibs', 'withdir', suffix='.git')
+        >>> r.repo_url
+        'git@github.com:tibs/withdir.git'
+
+    (although note that github will cope with or without the '.git' at the end).
+    Note that we had to specify the '.' in '.git', it wasn't assumed.
 
     Subversion allows retrieving *part* of a repository, by specifying the
     internal path leading to the entity to be retrieved. So, for instance:
 
       >>> r = Repository('svn+ssh://svn@project-server/opt/kynesim/projects/042/svn/',
       ...                'all_our_code', inner_path='core/busybox-1.18.4')
-      >>> r.path()
+      >>> r.repo_url
       'ssh://svn@project-server/opt/kynesim/projects/042/svn/all_our_code/core/busybox-1.18.4'
 
     If you specify more than one of 'extension' and 'inner_path', the result
@@ -86,35 +110,49 @@ class Repository(object):
     # and return a value for our 'path' method to return.
     path_handlers = {}
 
-    def __init__(self, base_path, co_name, prefix=None, extension=None, postfix=None,
-                 inner_path=None, revision=None, branch=None):
-        self.base_path = base_path
+    def __init__(self, given_path, co_name, prefix=None, suffix=None,
+                 inner_path=None, revision=None, branch=None, handler='guess'):
+        self.given_path = given_path
 
         # Work out our VCS
-        parts = base_path.split('+')
+        parts = given_path.split('+')
         if len(parts) == 1 or parts[0] == '':
-            raise GiveUp('Repository base_path must be <vcs>+<url>,'
-                         ' but got "%s"'%base_path)
+            raise GiveUp('Repository given_path must be <vcs>+<url>,'
+                         ' but got "%s"'%given_path)
 
         self.vcs = parts[0]
         self.repo = '+'.join(parts[1:])  # we'll hope that made sense
 
         self.co_name = co_name
         self.prefix = prefix
-        self.extension = extension
-        self.postfix = postfix
+        self.suffix = suffix
         self.inner_path = inner_path
         self.revision = revision
         self.branch = branch
 
+        # Yes, this is rather horrible...
+        if handler == 'guess':
+            if self.vcs == 'git' and self.repo.startswith('https://code.google.com/p/'):
+                handler = 'code.google.com'
+            else:
+                handler = None
+
+        if handler:
+            try:
+                handler_fn = self.path_handlers[(self.vcs, handler)]
+            except KeyError:
+                raise GiveUp('Cannot find %s "%s" handler, for %s'%(self.vcs,
+                             handler, self))
+            self.repo_url = handler_fn(self)
+        else:
+            self.repo_url = self.default_path()
+
     def __repr__(self):
-        parts = [repr(self.base_path), repr(self.co_name)]
+        parts = [repr(self.given_path), repr(self.co_name)]
         if self.prefix:
             parts.append('prefix=%s'%repr(self.prefix))
-        if self.extension:
-            parts.append('extension=%s'%repr(self.extension))
-        if self.postfix:
-            parts.append('postfix=%s'%repr(self.postfix))
+        if self.suffix:
+            parts.append('suffix=%s'%repr(self.suffix))
         if self.inner_path:
             parts.append('inner_path=%s'%repr(self.inner_path))
         if self.revision:
@@ -123,31 +161,23 @@ class Repository(object):
             parts.append('branch=%s'%repr(self.branch))
         return 'Repository(%s)'%(', '.join(parts))
 
-    def path(self):
-        """Return the repository path.
+    def default_path(self):
+        """Return the default repository path, calculated from all the parts.
 
         It is returned in a manner suitable for passing to the appropriate
         command line tool for cloning the repository.
-
-        MAYBE we should calculate this is __init__, and make it a value...
         """
-        for (vcs, starts), v in self.path_handlers.items():
-            if self.vcs == vcs and self.repo.startswith(starts):
-                raise NotImplementedError
-
         parts = [self.repo]
         if self.prefix:
             parts.append(self.prefix)
         parts.append(self.co_name)
-        if self.postfix:
-            parts.append(self.postfix)
-        # We trust and hope these next two don't clash!
+        # We trust and hope that inner_path doesn't clash with suffix...
         if self.inner_path:
             parts.append(self.inner_path)
         result = os.path.join(*parts)
 
-        if self.extension:
-            result = '%s%s'%(result, self.extension)
+        if self.suffix:
+            result = '%s%s'%(result, self.suffix)
 
         return result
 
@@ -157,10 +187,11 @@ class Repository(object):
 
         * 'vcs' is the short name of the VCS we're interested in, as returned
           by split_vcs_url().
-        * 'starts_with' is what a repository 'base_path' must start with in
+        * 'starts_with' is what a repository 'given_path' must start with in
           order for us to use the handler
         * 'handler' is a function that takes a Repository instance and
-          returns a path suitable for return by the Repository 'path()'
+          returns a path suitable for putting into the Repository instance's
+          'repo_url' value
           method.
 
         For instance, the google code handler for git might be registered
@@ -190,10 +221,26 @@ class Repository(object):
         return Repository.path_handlers.get((vcs, starts_with))
 
 def google_code_handler(repo):
-    return 'Fred'
+    if repo.vcs != 'git':
+        raise GiveUp('The code.google.com handler currently only understands'
+                     ' git, not %s, in %s'%(vcs, repo))
 
-Repository.register_path_handler('git', 'https://code.google.com/p/',
-                                 google_code_handler)
+    if repo.prefix:
+        raise GiveUp('The code.google.com handler does not support the'
+                     ' prefix value, in %s'%repo)
+    if repo.suffix:
+        raise GiveUp('The code.google.com handler does not support the'
+                     ' suffix value, in %s'%repo)
+    if repo.inner_path:
+        raise GiveUp('The code.google.com handler does not support the'
+                     ' inner_path value, in %s'%repo)
+
+    if repo.co_name == 'default':
+        return repo.repo
+    else:
+        return '%s.%s'%(repo.repo, repo.co_name)
+
+Repository.register_path_handler('git', 'code.google.com', google_code_handler)
 
 if __name__ == '__main__':
     import doctest

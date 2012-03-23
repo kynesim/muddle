fdf2bc584c8c23820f1042cb1086b5c786a2d187
Tony Ibbs <tibs@kynesim.co.uk>
2012-03-23 14:11:30 +0000

    Add new "upstream" argument to push and pull

--- muddled/version_control.py
+++ muddled/version_control.py
@@ -56,7 +56,7 @@ class VersionControlSystem(object):
         """
         pass
 
-    def pull(self, repo, options, verbose=True):
+    def pull(self, repo, options, upstream=None, verbose=True):
         """
         Will be called in the actual checkout's directory.
 
@@ -80,7 +80,7 @@ class VersionControlSystem(object):
         """
         pass
 
-    def push(self, repo, options, branch=None, verbose=True):
+    def push(self, repo, options, branch=None, upstream=None, verbose=True):
         """
         Will be called in the actual checkout's directory.
 
@@ -293,11 +293,14 @@ class VersionControlHandler(object):
                 raise utils.GiveUp('Failure checking out %s in %s:\n%s'%(self.checkout_label,
                                    parent_dir, err))
 
-    def pull(self, verbose=True):
+    def pull(self, upstream=None, verbose=True):
         """
         Retrieve changes from the remote repository, and apply them to
         the local working copy, but not if a merge operation would be
         required, in which case an exception shall be raised.
+
+        If 'upstream' is true, then it is the name of the repository as an
+        upstream.
         """
         if not self.repo.pull:
             raise utils.GiveUp('Failure pulling %s in %s:\n'
@@ -306,7 +309,8 @@ class VersionControlHandler(object):
 
         with utils.Directory(self.get_my_absolute_checkout_path()):
             try:
-                self.vcs_handler.pull(self.repo, self.options, verbose)
+                self.vcs_handler.pull(self.repo, self.options,
+                                      upstream=upstream, verbose=verbose)
             except utils.MuddleBug as err:
                 raise utils.MuddleBug('Error pulling %s in %s:\n%s'%(self.checkout_label,
                                   self.src_rel_dir(), err))
@@ -357,9 +361,12 @@ class VersionControlHandler(object):
                 raise utils.GiveUp('Failure commiting %s in %s:\n%s'%(self.checkout_label,
                                     self.src_rel_dir(), err))
 
-    def push(self, verbose=True):
+    def push(self, upstream=None, verbose=True):
         """
         Push changes in the local repository to the remote repository.
+
+        If 'upstream' is true, then it is the name of the repository as an
+        upstream.
 
         Note that in a centralised VCS, like subversion, this is typically
         called "commit", since there is no local repository.
@@ -373,7 +380,8 @@ class VersionControlHandler(object):
 
         with utils.Directory(self.get_my_absolute_checkout_path()):
             try:
-                self.vcs_handler.push(self.repo, self.options, verbose)
+                self.vcs_handler.push(self.repo, self.options,
+                                      upstream=upstream, verbose=verbose)
             except utils.MuddleBug as err:
                 raise utils.MuddleBug('Error pushing %s in %s:\n%s'%(self.checkout_label,
                                   self.src_rel_dir(), err))

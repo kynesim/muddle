40d672c99e9845691a89fb60095c21d388056c53
Tony Ibbs <tibs@kynesim.co.uk>
2013-04-26 15:48:22 +0100

    Improved run functionality

--- sandbox/run.py
+++ sandbox/run.py
@@ -4,11 +4,23 @@
 
 import sys
 import subprocess
+import shlex
 
-def main():
+def run(thing):
+    if isinstance(thing, basestring):
+        print '> %s'%thing
+        thing = shlex.split(thing)
+    else:
+        o = []
+        for item in thing:
+            if ' ' in item or '\t' in item:
+                o.append(repr(item))
+            else:
+                o.append(item)
+        print '> %s'%(' '.join(o))
     text = ''
     print '=================='
-    proc = subprocess.Popen(['ls'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    proc = subprocess.Popen(thing, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     for data in proc.stdout:
         sys.stdout.write(data)
         text += data
@@ -17,6 +29,12 @@ def main():
     print text,
     print '=================='
     print 'Return code', proc.returncode
+
+def main():
+    run('ls -l')
+    run(['ls', '-l'])
+    run('ls "fred jim"')
+    run(['ls', 'fred jim'])
 
 
 if __name__ == '__main__':

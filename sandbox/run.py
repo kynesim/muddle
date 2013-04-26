76a63bd6009dbb7ae08c0f9a9132bb7836bf1c11
Tony Ibbs <tibs@kynesim.co.uk>
2013-04-26 15:08:15 +0100

    Thinking on a better utils.py::run_cmd...

---
+++ sandbox/run.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+# This is what I want inside my utils.py::run_cmd...
+
+import sys
+import subprocess
+
+def main():
+    text = ''
+    print '=================='
+    proc = subprocess.Popen(['ls'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    for data in proc.stdout:
+        sys.stdout.write(data)
+        text += data
+    proc.wait()
+    print '=================='
+    print text
+
+
+if __name__ == '__main__':
+    main()
